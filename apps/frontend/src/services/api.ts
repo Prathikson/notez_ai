5e65054879b806bc1d41c1f0e36b283dde12440b
Prathikson <prathik@vincellstudios.com>
2025-05-12 18:46:33 -0600

    API Url fix

--- apps/frontend/src/services/api.ts
+++ apps/frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const apiUrl = "https://notez-backend-rs5g.onrender.com/"
+const apiUrl = "https://notez-backend-rs5g.onrender.com"
 
 export const uploadFile = async (file: File): Promise<{ audioUrl: string; transcription: string; summary: string }> => {
   const formData = new FormData();
